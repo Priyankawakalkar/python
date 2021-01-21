69b49831e60c7c7cb0197ee54c547d5d17a7e527
Priyankawakalkar <65772811+Priyankawakalkar@users.noreply.github.com>
2021-01-21 18:29:46 +0530

    Update program.py

--- program.py
+++ program.py
@@ -106,16 +106,6 @@ for i in range(num):
  count = count + 1
 print("list =",list)
 print("number of strings having same first and last character = ",count)
-Output:--
-Enter length of a list 5
-Enter elements of list
-123
-121
-zpz
-anu
-525
-list = ['123', '121', 'zpz', 'anu', '525']
-number of strings having same first and last character = 3 
 
 # 8)Write a program to get a list, sorted in increasing order by the last element in each tuple from a
 given list of non-empty tuples.
@@ -130,16 +120,7 @@ while (l1 != ''):
  list.append(tuple(map(int, l1.split())))
  l1 = input()
 print("list =",list)
-print("The Sorted list is =",sorted_list(list))
-Output:--
-Enter list of tuples
-1 5
-2 3
-9 8
-6 5
-7 0
-list = [(1, 5), (2, 3), (9, 8), (6, 5), (7, 0)]
-The Sorted list is = [(7, 0), (2, 3), (1, 5), (6, 5), (9, 8)] 
+print("The Sorted list is =",sorted_list(list)) 
 
 # 9)Write a program to remove duplicates elements from a list.
 Program:--
@@ -154,13 +135,44 @@ for element in list:
  if element not in list1:
  list1.append(element)
 print("list without duplicates =",list1)
+
+# 10)Write a program to check a list is empty or not.
+Program:--
+list = []
+no = int(input("Enter length of a list "))
+print("Enter elements of list ")
+for i in range(no):
+ l1 = int(input())
+ list.insert(i,l1)
+print("Enterred List is =",list)
+if len(list)== 0:
+ print("given list is empty")
+else:
+ print("given list is not empty")
+
+# 11)Write a program to find the list of words that are longer than n from a given list of words.
+Program:--
+def word(n, str):
+ wlen = []
+ text = str.split(" ")
+ for x in text:
+ if len(x) > n:
+ wlen.append(x)
+ return wlen 
+print(word(3, "Hello this is python programming"))
 Output:--
-Enter the length of a list 5
-Enter elements of list
-23
-56
-89
-23
-56
-list = [23, 56, 89, 23, 56]
-list without duplicates = [23, 56, 89] 
+['Hello', 'this', 'python', 'programming']
+# 12) Write a program to print the numbers of a specified list after removing even numbers
+from it.
+Program:--
+list = []
+no = int(input("Enter length of a list "))
+print("Enter elements of list ")
+for i in range(no):
+ l1 = int(input())
+ list.insert(i,l1)
+for i in list:
+ if(i%2 == 0):
+ list.remove(i)
+print("list after removing even numbers ")
+print(list)

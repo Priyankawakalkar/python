645059e04cf279bf2a354d2c47656dce5ff10101
Priyankawakalkar <65772811+Priyankawakalkar@users.noreply.github.com>
2021-01-24 19:39:42 +0530

    Update Day7-Day8.py

--- Day7-Day8.py
+++ Day7-Day8.py
@@ -1,1 +1,29 @@
-1.Write a Python program to get the frequency of the elements in a list.
+# 1.Write a Python program to get the frequency of the elements in a list.
+list = []
+no = int(input("Enter length of a list1 "))
+print("Enter elements of list1 ")
+for i in range(no):
+ l1 = int(input())
+ list.insert(i,l1)
+print("List is =",list)
+freq = {}
+for item in list:
+ if (item in freq):
+ freq[item] += 1
+ else:
+ freq[item] = 1
+for key, value in freq.items():
+ print("%s : %d" % (key, value))
+Output:--
+Enter length of a list1 6
+Enter elements of list1
+2
+3
+2
+5
+2
+3
+List is = [2, 3, 2, 5, 2, 3]
+2 : 3
+3 : 2
+5 : 1 

097a4cf2461fcccbf8c7b5fdc38eb6d2f031ffbf
Priyankawakalkar <65772811+Priyankawakalkar@users.noreply.github.com>
2021-01-25 21:02:09 +0530

    Update Day9-Day10.py

--- Day9-Day10.py
+++ Day9-Day10.py
@@ -10,3 +10,12 @@ dict4 ={}
 for d in(dict1,dict2,dict3):
  dict4.update(d)
 print("dictionary is ",dict4) 
+
+Output:--
+Enter dict1 {'1':10}
+dict1 is = {'1': 10}
+Enter dict2 {'2':20}
+dict2 is = {'2': 20}
+Enter dict3 {'3':30}
+dict3 is = {'3': 30}
+dictionary is {'1': 10, '2': 20, '3': 30}

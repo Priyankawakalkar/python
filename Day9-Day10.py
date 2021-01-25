2a35547f9e326e3735948346cf2ed691432ca8bc
Priyankawakalkar <65772811+Priyankawakalkar@users.noreply.github.com>
2021-01-25 21:08:56 +0530

    Update Day9-Day10.py

--- Day9-Day10.py
+++ Day9-Day10.py
@@ -36,9 +36,8 @@ dict is = {'1': 10, '2': 20}
 Enter key to check = 2
 key is already exist in dictionary 
 
-# 3) Write a Program to generate and print a dictionary that contains a number
-(between 1 and n) in the form (x, x*x).
-Program:--
+# 3) Write a Program to generate and print a dictionary that contains a number (between 1 and n) in the form (x, x*x).
+# Program:--
 dict ={}
 n = eval(input("Enter number = "))
 for x in range(1,n+1):

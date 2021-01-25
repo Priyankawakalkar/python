1da73e2b58193d2214f555f1aa0a188477f8b89f
Priyankawakalkar <65772811+Priyankawakalkar@users.noreply.github.com>
2021-01-25 21:01:38 +0530

    Create Day9-Day10.py

---
+++ Day9-Day10.py
@@ -0,0 +1,12 @@
+1.Write a program to concatenate following dictionaries to create a new one.
+Program:--
+dict1 = eval(input("Enter dict1 "))
+print("dict1 is =",dict1)
+dict2 = eval(input("Enter dict2 "))
+print("dict2 is =",dict2)
+dict3 = eval(input("Enter dict3 "))
+print("dict3 is =",dict3)
+dict4 ={}
+for d in(dict1,dict2,dict3):
+ dict4.update(d)
+print("dictionary is ",dict4) 

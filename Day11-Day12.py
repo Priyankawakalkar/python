98ffb86290f6ed2b4085ce13727c79c8ae2d059b
Priyankawakalkar <65772811+Priyankawakalkar@users.noreply.github.com>
2021-01-26 21:43:33 +0530

    Update Day11-Day12.py

--- Day11-Day12.py
+++ Day11-Day12.py
@@ -23,3 +23,16 @@ key 4
 value 20
 dictionary is = {1: 10, 2: 20, 3: 50, 4: 20}
 unique values in a dictionary = {10, 20, 50} 
+
+2.Write a program to create and display all combinations of letters, selecting each
+letter from a different key in a dictionary.
+Program:--
+dict = eval(input("Enter dict "))
+print("dict1 is =",dict)
+list1 = dict.get('1')
+list2 = dict.get('2')
+for i in range(2):
+ for j in range(2):
+ print(list1[i]+list2[j]) 
+
+ 

7ccd8a2bbd45c71e6a840c472f9f4d29a4c45d5b
Priyankawakalkar <65772811+Priyankawakalkar@users.noreply.github.com>
2021-01-26 21:40:57 +0530

    Update Day11-Day12.py

--- Day11-Day12.py
+++ Day11-Day12.py
@@ -8,3 +8,18 @@ for i in range(no):
  dict[key] =value
 print("dictionary is = ",dict)
 print("unique values in a dictionary = ",set(dict.values())) 
+
+Output:--
+Enter no of elements = 5
+key 1
+value 10
+key 2
+value 20
+key 1
+value 10
+key 3
+value 50
+key 4
+value 20
+dictionary is = {1: 10, 2: 20, 3: 50, 4: 20}
+unique values in a dictionary = {10, 20, 50} 

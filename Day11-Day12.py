16e2c64fa240d3bf3716a412b0d4488c7e0def44
Priyankawakalkar <65772811+Priyankawakalkar@users.noreply.github.com>
2021-01-27 17:48:22 +0530

    Update Day11-Day12.py

--- Day11-Day12.py
+++ Day11-Day12.py
@@ -71,3 +71,19 @@ highest 3 values in dictionary are {40, 50, 30}
 
 1.Write a Python program to combine values in python list of dictionaries. Go to the
 editor 
+Program:--
+d1=eval(input("Enter dict1 "))
+d2=eval(input("Enter dict2 "))
+d3=eval(input("Enter dict3 "))
+l1=[d1,d2,d3]
+print("list of dictionaries is",l1)
+list=[]
+for d in l1:
+ list.append(tuple(d.values()))
+dict={}
+for i in list:
+ if i[0] in dict:
+ dict[i[0]]+=i[1]
+ else:
+ dict[i[0]]=i[1]
+print("new dictionary is ",dict) 

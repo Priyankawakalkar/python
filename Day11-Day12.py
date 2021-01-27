6dde27aa0ac62381aec4fc365847e240913635fd
Priyankawakalkar <65772811+Priyankawakalkar@users.noreply.github.com>
2021-01-27 17:55:12 +0530

    Update Day11-Day12.py

--- Day11-Day12.py
+++ Day11-Day12.py
@@ -117,7 +117,7 @@ for x in str:
  count[x]=1
 print("count of the letters is ",count) 
 
-Output:--
+# Output:--
 Enter a string wakalkar
 String is wakalkar
 count of the letters is {'w': 1, 'a': 3, 'k': 2, 'l': 1, 'r': 1}

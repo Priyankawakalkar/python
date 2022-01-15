53c2da780dcd764473401425e7442a5d2ff32509
Priyankawakalkar <65772811+Priyankawakalkar@users.noreply.github.com>
2022-01-15 21:11:20 +0530

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -26,8 +26,7 @@ if string == string.title():
 else:
     print(False)
 
-    #5. Write python program to print all the duplicates from a given list without using set function
-
+#5. Write python program to print all the duplicates from a given list without using set function
 from collections import Counter
 l1 = [1,2,2,2,3,3,4,4,5]
 s1 = Counter(l1)
@@ -39,7 +38,6 @@ for key in s1:
 print(l2)
 
 #6. print symmetric diff bet two list
-
 l1=[1,2,8,9,6]
 l2=[1,6,5,4,3]
 print(set(l1).symmetric_difference(set(l2)))
